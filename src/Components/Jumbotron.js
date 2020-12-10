ef37aade13e9dc8674128ecd81c0c99068a63393
Argenis Dominguez <argenisdominguez@Argeniss-MacBook-Pro.local>
2020-12-10 16:03:08 -0500

    verify jenkins is doing his job

--- src/Components/Jumbotron.js
+++ src/Components/Jumbotron.js
@@ -3,7 +3,7 @@ import React from 'react'
 const jumbotron = () => {
     return (
         <div className="jumbotron text-white" style={{ background: 'none' }}>
-            <h1 className="display-4 mb-5">Bootleg Amazon !</h1>
+            <h1 className="display-4 mb-5">Bootleg Amazon</h1>
             <p className='mb-5'>Online marketplace to buy and sell locally! No need to visit a used car dealership or thrift store to find the best local deals! Here you’ll find a wide selection of used cars for sale, home goods, used electronics, vintage clothes, used furniture, used books, sports equipment, and real estate for sale or rent. It’s everything you can wish for!.</p>
             <p className="lead">
                 <a className="btn btn-warning btn-lg" href="" role="button">Learn more</a>
